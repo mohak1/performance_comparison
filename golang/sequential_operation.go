44091d89c8bc58616b69164d092cb11949b8c29b
Mohak Narang <narangmohak9@gmail.com>
2024-09-01 00:06:57 +0100

    adding 'validate_path_and_words' method

--- golang/sequential_operation.go
+++ golang/sequential_operation.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
+	"os"
 	"strings"
 )
 
 const FILES_DIR = "./text_files/smaller_files/" // dir where log files are present
 
-var WORDS_TO_SEARCH = [...]string{
+var WORDS_TO_SEARCH = []string{
 	"Session",
 	"Warning",
 	"Failed",
@@ -27,6 +29,34 @@ func perform_case_insensitive_search(
 	return
 }
 
+func validate_path_and_words() {
+	log.Print("Validating FILES_DIR...")
+	info, err := os.Stat(FILES_DIR)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatal("The location '%v' does not exist. Please "+
+				"check the path and try again.", FILES_DIR)
+		}
+		log.Fatal("Error checking the path: %v", FILES_DIR)
+	}
+	log.Print("%v is a valid path", FILES_DIR)
+
+	if info.IsDir() {
+		log.Print("%v is a valid directory", FILES_DIR)
+	} else {
+		log.Fatal("The location '%v' is not a directory. Please "+
+			"check the path and try again.", FILES_DIR)
+	}
+
+	if len(WORDS_TO_SEARCH) < 1 {
+		log.Fatal("WORDS_TO_SEARCH cannot be an empty list. Please add " +
+			"some words to this list and try again.")
+	}
+	log.Print("WORDS_TO_SEARCH list is not empty")
+
+}
+
+
 func main() {
-	perform_case_insensitive_search()
+	validate_path_and_words()
 }

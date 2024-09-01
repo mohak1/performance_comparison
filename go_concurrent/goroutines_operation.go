fbfda103baf1aa709ce85608dc91d2a2cf0957e3
Mohak Narang <narangmohak9@gmail.com>
2024-09-02 00:25:41 +0100

    removing extra string type casting

--- go_concurrent/goroutines_operation.go
+++ go_concurrent/goroutines_operation.go
@@ -30,7 +30,7 @@ func perform_case_insensitive_search(
 	for _, word := range words {
 		var lower_case_word = strings.ToLower(string(word))
 		var lower_case_line = strings.ToLower(string(line))
-		tracker[string(word)] += strings.Count(lower_case_line, lower_case_word)
+		tracker[word] += strings.Count(lower_case_line, lower_case_word)
 	}
 }
 
@@ -84,7 +84,7 @@ func process_file(
 	var start_time = time.Now()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := string(scanner.Text())
+		line := scanner.Text()
 		perform_case_insensitive_search(
 			words_to_search,
 			line,
